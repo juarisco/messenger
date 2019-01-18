54ac523b6d186942b3e1e372a07b3f0e554cc4d5
juarisco <juarisco@outlook.com>
2019-01-18 11:07:39 -0500

    Entidades y sus atributos

---
+++ database/migrations/2019_01_18_154716_create_conversations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateConversationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('conversations', function (Blueprint $table) {
+            $table->increments('id');
+
+            user
+                contact
+                last_message
+                listen_notifications
+                has_blocked
+
+                $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('conversations');
+    }
+}
