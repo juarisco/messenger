2afc40f3e1ae5c1b076818da0ccd23fd52c60974
juarisco <juarisco@outlook.com>
2019-01-25 10:46:30 -0500

    Broadcast-de-eventos

--- app/Observers/MessageObserver.php
+++ app/Observers/MessageObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Message;
 use App\Conversation;
+use App\Events\MessageSent;
 
 class MessageObserver
 {
@@ -30,5 +31,7 @@ class MessageObserver
             $conversation->last_time = $message->created_at;
             $conversation->save();
         }
+
+        event(new MessageSent($message));
     }
 }
